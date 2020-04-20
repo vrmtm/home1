940bc34cca9f5ccf8c8d37e1a065248ab2c18c8f
vrm <mr.vrm.tm@gmail.com>
2020-04-20 21:25:41 +0300

    .

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
@@ -18,10 +18,6 @@ module.exports = {
   ],
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
       {
         test: /\.(js|jsx)$/,
         exclude: /[\\/]node_modules[\\/]/,
