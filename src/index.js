55bcbaa8c25ae3a9ae91b45b1e84c2e026ab0d60
vrm <mr.vrm.tm@gmail.com>
2020-04-23 13:40:42 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,60 @@
 import './index.css';
+import 'particles.js';
 
-const p = document.createElement('p');
-p.textContent = 'GG';
-document.getElementById('app').append(p);
+let partQty = window.innerWidth / 10;
+particlesJS('app', {
+  particles: {
+    number: {
+      value: partQty,
+    },
+    color: {
+      value: '#ffffff',
+    },
+    shape: {
+      type: 'circle',
+    },
+    opacity: {
+      value: 1,
+    },
+    size: {
+      value: 2,
+    },
+    line_linked: {
+      enable_auto: true,
+      distance: 150,
+      color: '#fff',
+      opacity: 0.5,
+      width: 1,
+      condensed_mode: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600,
+      },
+    },
+    move: {
+      enable: true,
+      speed: 2,
+    },
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'grab',
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 200,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+    },
+  },
+});
